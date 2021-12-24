e4a91bbccc3548502d0d1db64c2971cbf0756e2d
samdial69 <sdiallo585gmail.com>
2021-12-24 21:23:13 +0100

    General class Decorator for Bille

--- exodecorateur_angryballs/maladroit/modele/DecorateurBille.java
+++ exodecorateur_angryballs/maladroit/modele/DecorateurBille.java
@@ -1,4 +1,51 @@
 package exodecorateur_angryballs.maladroit.modele;
 
-public class DecorateurBille {
+import mesmaths.geometrie.base.Vecteur;
+
+import java.awt.*;
+
+public abstract class DecorateurBille extends Bille {
+    protected Bille billeADecorer;
+
+    public DecorateurBille(Bille billeADecorer) {
+        this.billeADecorer = billeADecorer;
+    }
+
+    @Override
+    public Vecteur getPosition() {
+        return this.billeADecorer.getPosition();
+    }
+
+    @Override
+    public double getRayon() {
+        return this.billeADecorer.getRayon();
+    }
+
+    @Override
+    public Vecteur getVitesse() {
+        return this.billeADecorer.getVitesse();
+    }
+
+    @Override
+    public Vecteur getAcceleration() {
+        return this.billeADecorer.getAcceleration();
+    }
+
+    @Override
+    public Color getCouleur() {
+        return this.billeADecorer.getCouleur();
+    }
+
+    @Override
+    public int getClef() {
+        return this.billeADecorer.getClef();
+    }
+
+    public Bille getBilleADecorer() {
+        return billeADecorer;
+    }
+
+    public void setBilleADecorer(Bille billeADecorer) {
+        this.billeADecorer = billeADecorer;
+    }
 }
