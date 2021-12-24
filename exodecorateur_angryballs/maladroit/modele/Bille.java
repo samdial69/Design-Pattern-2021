67fe4f2305efadf33f7b6d82c0ffeee0a48c2664
samdial69 <sdiallo585gmail.com>
2021-12-24 21:19:37 +0100

    Rewrite of Bille class by splitting the existant class for removing propoerties def to an class explicit

--- exodecorateur_angryballs/maladroit/modele/Bille.java
+++ exodecorateur_angryballs/maladroit/modele/Bille.java
@@ -16,194 +16,87 @@ import mesmaths.geometrie.base.Vecteur;
  *  
  * 
  * */
-public abstract class Bille
-{
-//----------------- classe Bille-------------------------------------
+public abstract class Bille {
+    //----------------- classe Bille-------------------------------------
+    private static int prochaineClef = 0;
 
-public  Vecteur position;   // centre de la bille
-public  double rayon;            // rayon > 0
-public  Vecteur vitesse;
-public  Vecteur accélération;
-public int clef;                // identifiant unique de cette bille
+    public static double ro = 1;        // masse volumique
 
-private Color couleur;
+    public abstract Vecteur getPosition();
 
-private static int prochaineClef = 0;
+    public abstract double getRayon();
 
-public static double ro = 1;        // masse volumique
+    public abstract Vecteur getVitesse();
 
+    public abstract Vecteur getAcceleration();
 
-/**
- * @param centre
- * @param rayon
- * @param vitesse
- * @param accélération
- * @param couleur
- */
-protected Bille(Vecteur centre, double rayon, Vecteur vitesse,
-        Vecteur accélération, Color couleur)
-{
-this.position = centre;
-this.rayon = rayon;
-this.vitesse = vitesse;
-this.accélération = accélération;
-this.couleur = couleur;
-this.clef = Bille.prochaineClef ++;
-}
+    public abstract Color getCouleur();
 
-/**
- * @param position
- * @param rayon
- * @param vitesse
- * @param couleur
- */
-public Bille(Vecteur position, double rayon, Vecteur vitesse, Color couleur)
-{
-this(position,rayon,vitesse,new Vecteur(),couleur);
-}
+    public abstract int getClef();
 
-
-
-/**
- * @return the position
- */
-public Vecteur getPosition()
-{
-return this.position;
-}
-
-
-
-
-/**
- * @return the rayon
- */
-public double getRayon()
-{
-return this.rayon;
-}
-
-
-
-
-/**
- * @return the vitesse
- */
-public Vecteur getVitesse()
-{
-return this.vitesse;
-}
-
-
-
-
-/**
- * @return the accélération
- */
-public Vecteur getAccélération()
-{
-return this.accélération;
-}
-
-
-
-
-/**
- * @return the clef
- */
-public int getClef()
-{
-return this.clef;
-}
-
-
-
-
-
-
-
-
-public double masse() {return ro*Geop.volumeSphère(rayon);}
-
-
-
-/**
- * mise à jour de position et vitesse à t+deltaT à partir de position et vitesse à l'instant t
- * 
- * modifie le vecteur position et le vecteur vitesse
- * 
- * laisse le vecteur accélération intact
- *
- * La bille subit par défaut un mouvement uniformément accéléré
- * */
-public  void  déplacer( double deltaT)
-{
-Cinematique.mouvementUniformémentAccéléré( this.getPosition(), this.getVitesse(), this.getAccélération(), deltaT);
-}
-
-/**
- * calcul (c-à-d mise à jour) éventuel  du vecteur accélération. 
- * billes est la liste de toutes les billes en mouvement
- * Cette méthode peut avoir besoin de "billes" si this subit l'attraction gravitationnelle des autres billes
- * La nature du calcul du vecteur accélération de la bille  est définie dans les classes dérivées
- * A ce niveau le vecteur accélération est mis à zéro (c'est à dire pas d'accélération)
- * */
-public  void gestionAccélération(Vector<Bille> billes)
-{
-this.getAccélération().set(Vecteur.VECTEURNUL);
-}
-
-/**
- * gestion de l'éventuelle  collision de cette bille avec les autres billes
- *
- * billes est la liste de toutes les billes en mouvement
- * 
- * Le comportement par défaut est le choc parfaitement élastique (c-à-d rebond sans amortissement)
- * 
- * @return true si il y a collision et dans ce cas les positions et vecteurs vitesses des 2 billes impliquées dans le choc sont modifiées
- * si renvoie false, il n'y a pas de collision et les billes sont laissées intactes 
- * */
-public boolean gestionCollisionBilleBille(Vector<Bille> billes)
-{
-return OutilsBille.gestionCollisionBilleBille(this, billes);
-}
-
-
-
-
-/**
- * gestion de l'éventuelle collision de la bille (this) avec le contour rectangulaire de l'écran défini par (abscisseCoinHautGauche, ordonnéeCoinHautGauche, largeur, hauteur)
- * 
- * détecte si il y a collision et le cas échéant met à jour position et vitesse
- * 
- * La nature du comportement de la bille en réponse à cette collision est définie dans les classes dérivées
- * */
-public abstract void collisionContour(double abscisseCoinHautGauche, double ordonnéeCoinHautGauche, double largeur, double hauteur);
-
-
-
-public void dessine (Graphics g)
-    {
-    int width, height;
-    int xMin, yMin;
-    
-    xMin = (int)Math.round(position.x-rayon);
-    yMin = (int)Math.round(position.y-rayon);
-
-    width = height = 2*(int)Math.round(rayon); 
-
-    g.setColor(couleur);
-    g.fillOval( xMin, yMin, width, height);
-    g.setColor(Color.CYAN);
-    g.drawOval(xMin, yMin, width, height);
+    public double masse() {
+        return ro * Geop.volumeSphère(this.getRayon());
     }
 
+    public void dessine(Graphics g) {
+        int width, height;
+        int xMin, yMin;
 
-public String toString() 
-    {
-    return "centre = " + position + " rayon = "+rayon +  " vitesse = " + vitesse + " accélération = " + accélération + " couleur = " + couleur + "clef = " + clef;
+        xMin = (int) Math.round(this.getPosition().x - this.getRayon());
+        yMin = (int) Math.round(this.getPosition().y - this.getRayon());
+
+        width = height = 2 * (int) Math.round(this.getRayon());
+
+        g.setColor(this.getCouleur());
+        g.fillOval(xMin, yMin, width, height);
+        g.setColor(Color.CYAN);
+        g.drawOval(xMin, yMin, width, height);
     }
 
-//----------------- classe Bille -------------------------------------
+    /**
+     * mise à jour de position et vitesse à t+deltaT à partir de position et vitesse à l'instant t
+     * modifie le vecteur position et le vecteur vitesse
+     * laisse le vecteur accélération intact
+     * La bille subit par défaut un mouvement uniformément accéléré
+     */
+    public void deplacer(double deltaT) {
+        Cinematique.mouvementUniformementAccelere(this.getPosition(), this.getVitesse(), this.getAcceleration(), deltaT);
+    }
+
+    /**
+     * calcul (c-à-d mise à jour) éventuel  du vecteur accélération.
+     * billes est la liste de toutes les billes en mouvement
+     * Cette méthode peut avoir besoin de "billes" si this subit l'attraction gravitationnelle des autres billes
+     * La nature du calcul du vecteur accélération de la bille  est définie dans les classes dérivées
+     * A ce niveau le vecteur accélération est mis à zéro (c'est à dire pas d'accélération)
+     */
+    public void gestionAcceleration(Vector<Bille> billes) {
+        this.getAcceleration().set(Vecteur.VECTEURNUL);
+    }
+
+    /**
+     * gestion de l'éventuelle collision de la bille (this) avec le contour rectangulaire de l'écran défini par (abscisseCoinHautGauche, ordonnéeCoinHautGauche, largeur, hauteur)
+     * détecte si il y a collision et le cas échéant met à jour position et vitesse
+     * La nature du comportement de la bille en réponse à cette collision est définie dans les classes dérivées
+     */
+    public abstract void collisionContour(double abscisseCoinHautGauche, double ordonneeCoinHautGauche, double largeur, double hauteur);
+
+
+    public String toString() {
+        return "centre = " + this.getPosition() + " rayon = " + this.getRayon() + " vitesse = " + this.getVitesse() + " accélération = " + this.getAcceleration() + " couleur = " + this.getCouleur() + "clef = " + this.getClef();
+
+    }
+
+    /**
+     * gestion de l'éventuelle  collision de cette bille avec les autres billes
+     * billes est la liste de toutes les billes en mouvement
+     * Le comportement par défaut est le choc parfaitement élastique (c-à-d rebond sans amortissement)
+     * @return true
+     * si il y a collision et dans ce cas les positions et vecteurs vitesses des 2 billes impliquées dans le choc sont modifiées
+     * si renvoie false, il n'y a pas de collision et les billes sont laissées intactes
+     */
+    public boolean gestionCollisionBilleBille(Vector<Bille> billes) {
+        return OutilsBille.gestionCollisionBilleBille(this, billes);
+    }
 }
 
